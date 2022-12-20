3fa6c68e6a99f16a876c9502982ac0d83c7fcbfc
nhvy <91833887+nhvy@users.noreply.github.com>
2022-12-20 22:43:58 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,27 +1,22 @@
-#include "holberton.h"
-#include <studio.h>
+#include <stdio.h>
+#include "main.h"
 
 /**
- * print_array - prints n elements of an array of integers,
- * followed by a new line.
- * @a: the array to be printed.
- * @n: the number of elements of the array to be printed.
- *
- * Return: nothing.
+ * print_array - prints n elements of an array of integers
+ * followed by a new line
+ * @a: array to be printed
+ * @n: number of elements to print
  */
 void print_array(int *a, int n)
 {
-	int i = 0, j = 0;
+	int i;
 
-	for (; a[j] != '\0'; j++)
-		;
-
-	for (; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		if (i == 0)
 			printf("%d", a[i]);
 		else
 			printf(", %d", a[i]);
 	}
-	printf("\n");
+		printf("\n");
 }

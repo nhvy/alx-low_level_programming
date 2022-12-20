0a8c1bf5878f2c08a044491d9ff49c64cd0bdef4
nhvy <91833887+nhvy@users.noreply.github.com>
2022-12-20 15:44:44 +0100

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to the standard output.
+ * @str: the string to be printed.
+ *
+ * Return: nothing.
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
+
+Footer
+© 2022 GitHub, Inc.

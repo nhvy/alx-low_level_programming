db8b1f9e7351927899a40c763d37ecf3288c5201
nhvy <91833887+nhvy@users.noreply.github.com>
2022-12-20 13:41:56 +0100

    Create 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers.
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: void.
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *b;
+	*b = *a;
+	*a = tmp;
+}
+Footer
+© 2022 GitHub, Inc.
